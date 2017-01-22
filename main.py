9113adad18ed0a735d3f48739dc3c010c9bd47d3
Stephen Brimhall <stephen.brimhall@icloud.com>
2017-01-22 15:43:19 -0800

    Added main.py file and src/server.py file

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+#
+# main.py
+#
+# Created by Stephen Brimhall on 1/22/17
+# Copyright (c) SST CTF and Stephen Brimhall. All Rights Reserved.
+#
+# Implements the primary server of the SST CTF chat network system.
+# This file contains the startup for the server.
+#
+
+# Begin module imports
+import socket;
+import sys;
+import os;
+# End module imports
+
+# Begin user imports
+import src.server
+
+# Begin functions
